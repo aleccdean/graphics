99e1013895b8224899f551765ff49d88a994d987
Alec Dean <deanac@dukes.jmu.edu>
2025-09-04 08:18:47 -0400

    rastercaster interpolate

--- apps/rastercaster/flat-fragment.glsl
+++ apps/rastercaster/flat-fragment.glsl
@@ -1,10 +1,11 @@
+in vec3 fragColor;
 out vec4 fragmentColor;
 uniform vec2 dimensions; 
 uniform vec2 mouse;
 uniform float time;
 
 vec3 f() {
-  vec3 color = vec3(gl_FragCoord.x / dimensions.x ,gl_FragCoord.y / dimensions.y , 1.0);
+  vec3 color = vec3(gl_FragCoord.x / dimensions.x * fragColor.x,gl_FragCoord.y / dimensions.y * fragColor.y, fragColor.z);
   return color;
 } 
 
--- apps/rastercaster/flat-vertex.glsl
+++ apps/rastercaster/flat-vertex.glsl
@@ -1,10 +1,10 @@
 in vec3 position;
-// in vec3 color; 
-// out vec3 mixColor;
+in vec3 color; 
+out vec3 fragColor;
 
 
 void main() {
   gl_Position = vec4(position, 1.0);
   gl_PointSize = 10.0;
- //  mixColor = color;
+  fragColor = color;
 }
